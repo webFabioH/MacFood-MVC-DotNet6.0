4e80fcf531f2eabb7c1a0e5d185a82de4c551eca
Fabio Miranda <web.jrhk@gmail.com>
2024-06-29 21:12:03 -0300

    Implemented the dataannotations and continuing the preparation for the migration

--- MacFood/Context/AppDbContext.cs
+++ MacFood/Context/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace MacFood.Context
+﻿using MacFood.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MacFood.Context
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Food> Foods { get; set; }
     }
 }
--- MacFood/Program.cs
+++ MacFood/Program.cs
@@ -1,6 +1,13 @@
+using MacFood.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContextPool<AppDbContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
