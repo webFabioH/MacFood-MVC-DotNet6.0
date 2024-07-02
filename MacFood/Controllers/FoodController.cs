4ab05d77c113d4dadb407d7baf965e97395a6293
Fabio Miranda <web.jrhk@gmail.com>
2024-07-01 22:35:54 -0300

    Created the controller to the view for to show the components and adjusted the view List code

---
+++ MacFood/Controllers/FoodController.cs
@@ -0,0 +1,21 @@
+﻿using MacFood.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MacFood.Controllers
+{
+    public class FoodController : Controller
+    {
+        private readonly IFoodRepository _foodRepository;
+
+        public FoodController(IFoodRepository foodRepository)
+        {
+            _foodRepository = foodRepository;
+        }
+
+        public IActionResult List()
+        {
+            var foods = _foodRepository.Foods;
+            return View(foods);
+        }
+    }
+}
