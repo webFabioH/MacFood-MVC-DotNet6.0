afb3a8816310ed4897b56d6507fb332e4ef6fda2
Fabio Miranda <web.jrhk@gmail.com>
2024-07-02 18:57:54 -0300

    Implemented the methods to Add, Remove, Clear, GetPurchaseItens, GetTotalPurchase to the class PurchaseCart

--- MacFood/Models/PurchaseCart.cs
+++ MacFood/Models/PurchaseCart.cs
@@ -1,4 +1,5 @@
 ﻿using MacFood.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace MacFood.Models
 {
@@ -28,6 +29,83 @@ namespace MacFood.Models
             {
                 PurchaseCartId = cartId
             };
+        }
+
+        public void AddToCart(Food food)
+        {
+            var purchaseCartItem = _context.PurchaseCartItems.SingleOrDefault(
+                    s => s.Food.FoodId == food.FoodId &&
+                    s.PurchaseCartId == PurchaseCartId
+                );
+
+            if (purchaseCartItem == null)
+            {
+                purchaseCartItem = new PurchaseCartItem
+                {
+                    PurchaseCartId = PurchaseCartId,
+                    Food = food,
+                    Quantity = 1
+                };
+                _context.PurchaseCartItems.Add(purchaseCartItem);
+            }
+            else
+            {
+                purchaseCartItem.Quantity++;
+            }
+            _context.SaveChanges();
+        }
+
+        public int RemoveToCart(Food food)
+        {
+            var purchaseCartItem = _context.PurchaseCartItems.SingleOrDefault(
+                    s => s.Food.FoodId == food.FoodId &&
+                    s.PurchaseCartId == PurchaseCartId
+                );
+
+            var localQuantity = 0;
+
+            if (purchaseCartItem != null)
+            {
+                if (purchaseCartItem.Quantity > 1)
+                {
+                    purchaseCartItem.Quantity--;
+                    localQuantity = purchaseCartItem.Quantity;
+                }
+                else
+                {
+                    _context.PurchaseCartItems.Remove(purchaseCartItem);
+                }
+            }
+
+            _context.SaveChanges();
+            return localQuantity;
+        }
+
+        public List<PurchaseCartItem> GetPurchaseCartItems() 
+        {
+            return PurchaseCartItem ?? (PurchaseCartItem =
+                                        _context.PurchaseCartItems
+                                        .Where(c => c.PurchaseCartId == PurchaseCartId)
+                                        .Include(s => s.Food)
+                                        .ToList());
+        }
+
+        public void CleanCart()
+        {
+            var cartItem = _context.PurchaseCartItems
+                .Where(x => x.PurchaseCartId == PurchaseCartId);
+
+            _context.PurchaseCartItems.RemoveRange(cartItem);
+            _context.SaveChanges();
+        }
+
+        public decimal GetTotalPurchase()
+        {
+            var totalPurchase = _context.PurchaseCartItems
+                                .Where(c => c.PurchaseCartId == PurchaseCartId)
+                                .Select(c => c.Food.Value * c.Quantity).Sum();
+
+            return totalPurchase;
         }
 
     }
