4901352de51829514824c0f57b9151def6ac8422
Fabio Miranda <web.jrhk@gmail.com>
2024-07-25 17:19:11 -0300

    Implemented the ViewComponent PurchaseCartSummary and updated the main page layout

---
+++ MacFood/Components/PurchaseCartSummary.cs
@@ -0,0 +1,35 @@
+﻿using MacFood.Models;
+using MacFood.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MacFood.Components
+{
+    public class PurchaseCartSummary : ViewComponent
+    {
+        private readonly PurchaseCart _purchaseCart;
+
+        public PurchaseCartSummary(PurchaseCart purchaseCart)
+        {
+            _purchaseCart = purchaseCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _purchaseCart.GetPurchaseCartItens();
+            //var itens = new List<PurchaseCartItem>()
+            //{
+            //    new PurchaseCartItem(),
+            //    new PurchaseCartItem()
+            //};
+            _purchaseCart.PurchaseCartItem = itens;
+
+            var purchaseCartVM = new PurchaseCartViewModel
+            {
+                PurchaseCart = _purchaseCart,
+                PurchaseCartTotal = _purchaseCart.GetTotalPurchase()
+            };
+
+            return View(purchaseCartVM);
+        }
+    }
+}
