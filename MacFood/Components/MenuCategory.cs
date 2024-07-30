73b0b6d86afb056441cef4394382757025d0c795
Fabio Miranda <web.jrhk@gmail.com>
2024-07-29 21:15:23 -0300

    Created the class and the view MenuCategory and implemented the dropdown to show the foods categories

---
+++ MacFood/Components/MenuCategory.cs
@@ -0,0 +1,21 @@
+﻿using MacFood.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MacFood.Components
+{
+    public class MenuCategory : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public MenuCategory(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories= _categoryRepository.Categories.OrderBy(c => c.CategoryName);
+            return View(categories);
+        }
+    }
+}
