3c72bdaa93407beabacd16e7229c019e857027f5
Fabio Miranda <web.jrhk@gmail.com>
2024-10-03 19:52:20 -0300

    Created and Implemented the LoginViewModel e AccountController

---
+++ MacFood/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MacFood.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Type your name")]
+        [Display(Name = "User")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Type your password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
